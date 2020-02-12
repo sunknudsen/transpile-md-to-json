491ff98293b1cdf70f6fc13736aec83d681ede50
Sun Knudsen <hello@sunknudsen.com>
2020-02-12 15:57:25 -0500

    Implemented Prettier and slugify
    Implemented Prettier for style and slugify to slugify directory and file names enforcing a predictable naming convention for JSON properties.

--- build/index.js
+++ build/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var __asyncValues = (this && this.__asyncValues) || function (o) {
     if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
     var m = o[Symbol.asyncIterator], i;
@@ -15,36 +15,47 @@ const chokidar_1 = __importDefault(require("chokidar"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 const readdirp_1 = __importDefault(require("readdirp"));
+const slugify_1 = __importDefault(require("slugify"));
 const dot_prop_1 = __importDefault(require("dot-prop"));
 const chalk_1 = __importDefault(require("chalk"));
 commander_1.default
-    .requiredOption('-s, --src <source>', 'path to markdown folder')
-    .requiredOption('-d, --dest <destination>', 'path to JSON file')
-    .option('-w, --watch', 'watch source for changes');
+    .requiredOption("-s, --src <source>", "path to markdown folder")
+    .requiredOption("-d, --dest <destination>", "path to JSON file")
+    .option("-w, --watch", "watch source for changes");
 commander_1.default.parse(process.argv);
 const src = path_1.default.resolve(process.cwd(), commander_1.default.src);
 const dest = path_1.default.resolve(process.cwd(), commander_1.default.dest);
 if (commander_1.default.watch) {
-    chokidar_1.default.watch(`${src}/**/*.md`, {
-        ignoreInitial: true
-    }).on('all', (event, path) => {
+    chokidar_1.default
+        .watch(`${src}/**/*.md`, {
+        ignoreInitial: true,
+    })
+        .on("all", (event, path) => {
         run();
     });
 }
 const run = async function () {
     var e_1, _a;
     let options = {
-        fileFilter: '*.md'
+        fileFilter: "*.md",
     };
     let markdown = {};
     try {
-        console.info('Transpiling...');
+        console.info("Transpiling...");
         try {
             for (var _b = __asyncValues(readdirp_1.default(src, options)), _c; _c = await _b.next(), !_c.done;) {
                 const file = _c.value;
-                let regExp = new RegExp(path_1.default.sep, 'g');
-                let dots = file.path.replace(regExp, '.').replace('.md', '');
-                dot_prop_1.default.set(markdown, dots, fs_1.default.readFileSync(path_1.default.resolve(src, file.path), 'utf8'));
+                let parts = file.path.split(path_1.default.sep);
+                parts.forEach(function (part, index) {
+                    parts[index] = slugify_1.default(part, {
+                        lower: true,
+                    });
+                });
+                let dots = parts
+                    .join(path_1.default.sep)
+                    .replace(new RegExp(path_1.default.sep, "g"), ".")
+                    .replace(/\.md$/, "");
+                dot_prop_1.default.set(markdown, dots, fs_1.default.readFileSync(path_1.default.resolve(src, file.path), "utf8"));
             }
         }
         catch (e_1_1) { e_1 = { error: e_1_1 }; }
@@ -55,7 +66,7 @@ const run = async function () {
             finally { if (e_1) throw e_1.error; }
         }
         fs_1.default.writeFileSync(dest, JSON.stringify(markdown, null, 2));
-        console.info(chalk_1.default.green('Transpiled successfully!'));
+        console.info(chalk_1.default.green("Transpiled successfully!"));
     }
     catch (error) {
         console.error(error);
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,64 @@
-'use strict';
+"use strict"
 
-import program from 'commander';
-import chokidar from 'chokidar';
-import path from 'path';
-import fs from 'fs';
-import readdirp from 'readdirp';
-import dotProp from 'dot-prop';
-import chalk from 'chalk';
+import program from "commander"
+import chokidar from "chokidar"
+import path from "path"
+import fs from "fs"
+import readdirp from "readdirp"
+import slugify from "slugify"
+import dotProp from "dot-prop"
+import chalk from "chalk"
 
 program
-  .requiredOption('-s, --src <source>', 'path to markdown folder')
-  .requiredOption('-d, --dest <destination>', 'path to JSON file')
-  .option('-w, --watch', 'watch source for changes')
+  .requiredOption("-s, --src <source>", "path to markdown folder")
+  .requiredOption("-d, --dest <destination>", "path to JSON file")
+  .option("-w, --watch", "watch source for changes")
 
-program.parse(process.argv);
+program.parse(process.argv)
 
-const src = path.resolve(process.cwd(), program.src);
-const dest = path.resolve(process.cwd(), program.dest);
+const src = path.resolve(process.cwd(), program.src)
+const dest = path.resolve(process.cwd(), program.dest)
 
 if (program.watch) {
-  chokidar.watch(`${src}/**/*.md`, {
-    ignoreInitial: true
-  }).on('all', (event, path) => {
-    run();
-  });
+  chokidar
+    .watch(`${src}/**/*.md`, {
+      ignoreInitial: true,
+    })
+    .on("all", (event, path) => {
+      run()
+    })
 }
 
 const run = async function() {
   let options = {
-    fileFilter: '*.md'
+    fileFilter: "*.md",
   }
-  let markdown = {};
+  let markdown = {}
   try {
-    console.info('Transpiling...');
+    console.info("Transpiling...")
     for await (const file of readdirp(src, options)) {
-      let regExp = new RegExp(path.sep, 'g');
-      let dots = file.path.replace(regExp, '.').replace('.md', '');
-      dotProp.set(markdown, dots, fs.readFileSync(path.resolve(src, file.path), 'utf8'));
+      let parts = file.path.split(path.sep)
+      parts.forEach(function(part, index) {
+        parts[index] = slugify(part, {
+          lower: true,
+        })
+      })
+      let dots = parts
+        .join(path.sep)
+        .replace(new RegExp(path.sep, "g"), ".")
+        .replace(/\.md$/, "")
+      dotProp.set(
+        markdown,
+        dots,
+        fs.readFileSync(path.resolve(src, file.path), "utf8")
+      )
     }
-    fs.writeFileSync(dest, JSON.stringify(markdown, null, 2));
-    console.info(chalk.green('Transpiled successfully!'));
+    fs.writeFileSync(dest, JSON.stringify(markdown, null, 2))
+    console.info(chalk.green("Transpiled successfully!"))
   } catch (error) {
-    console.error(error);
-    process.exit(1);
+    console.error(error)
+    process.exit(1)
   }
 }
 
-run();
+run()
